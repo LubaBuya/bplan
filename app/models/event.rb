123269abd421502792a4ca7c0f5a50bc17086135
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-01 14:50:54 -0800

    annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  event_type  :string(255)
+#  start_at    :datetime
+#  end_at      :datetime
+#  location    :string(255)
+#  description :text
+#  department  :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Event < ActiveRecord::Base
 end
