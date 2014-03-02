1081dbd16bfdee281b83cba2571601be474cb2f7
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-02 03:05:12 -0800

    use current time

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
 
   def index
     Time.zone = 'Pacific Time (US & Canada)'
-    d = Time.new(2014, 03, 1)
-    # d = Time.now
+    #d = Time.new(2014, 03, 1)
+    d = Time.now
     # d = d.at_beginning_of_day + 2
     
     @events_today = Event.all.select {|x| x.end_at > d && x.end_at <= d.at_end_of_day }.sort {|a,b| a.start_at <=> b.start_at }
