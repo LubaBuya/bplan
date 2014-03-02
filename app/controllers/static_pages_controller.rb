b96163503af1a9cc0872cba7a73fde680ae43087
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-01 23:17:45 -0800

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def index
-    Time.zone = 'Pacific Time (US & Canada)' 
-    d = Time.now
-    d = d.at_beginning_of_day + 2
+    Time.zone = 'Pacific Time (US & Canada)'
+    d = Time.new(2014, 03, 1)
+    # d = Time.now
+    # d = d.at_beginning_of_day + 2
     
     @events_today = Event.all.select {|x| x.end_at > d && x.end_at <= d.at_end_of_day }.sort {|a,b| a.start_at <=> b.start_at }
     @events_upcoming = Event.all.select {|x| x.end_at > d.at_end_of_day }.sort {|a,b| a.start_at <=> b.start_at }
