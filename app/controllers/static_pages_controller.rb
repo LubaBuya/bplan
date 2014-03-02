486ba3d4920ef95746178cfaaaec2fcd4c6c2e2b
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-01 23:16:45 -0800

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,8 @@ class StaticPagesController < ApplicationController
     
     @events_today = Event.all.select {|x| x.end_at > d && x.end_at <= d.at_end_of_day }.sort {|a,b| a.start_at <=> b.start_at }
     @events_upcoming = Event.all.select {|x| x.end_at > d.at_end_of_day }.sort {|a,b| a.start_at <=> b.start_at }
+
+    @events_upcoming = @events_upcoming[0..20]
   end
 
 end
