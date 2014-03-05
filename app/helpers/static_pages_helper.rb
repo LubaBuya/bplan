98d3fb9a2fb985ebef5c6ed074e237eec8e890cc
Derrick Mar <derrickmar1215@berkeley.edu>
2014-03-05 03:27:46 -0800

    styling stuff

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,11 @@
 module StaticPagesHelper
 
-  def big_event_style(e)
-    "border: 1px solid #{e.group.color};" +
-    "border-left: 8px solid #{e.group.color};"
-  end
-  
+	def big_event_style(e)
+		"border: 2px solid #{e.group.color};"
+	end
+
+	# def tag(e)
+	# 	"background-color: #{e.group.color};"
+	# end
+
 end
