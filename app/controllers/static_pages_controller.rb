4e993db867bd4a8f462ec7846fa87791f0feaeb2
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-06 21:08:56 -0800

    better database queries, no duplicate events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,41 +8,39 @@ class StaticPagesController < ApplicationController
     Time.zone = 'Pacific Time (US & Canada)'
     d = Time.now.in_time_zone(Time.zone)
     
-    # selecting only events that are today. First getting events that are today. Then we are sorting it by comparing each one?
-    @events_today = Event.order(:start_at).select {|x| x.end_at > d && x.end_at <= d.at_end_of_day }
+    @events_today = Event.where(end_at: d..d.at_end_of_day).order(:start_at, :title)
 
     @left_name = "Today"
-
     @user = current_user
-
+    
     if not @user.blank?
-      groups = Set.new(@user.groups.map(&:id))
-      @events_today.select! {|e| groups.include? e.group.id }
+      groups = @user.groups.map(&:id)
+      @events_today = @events_today.where(group_id: groups)
     end
 
-    if @events_today.count == 0
+    @events_today = @events_today.group(:title)
+    
+    if @events_today.length == 0
       d = (d + 1.day).at_beginning_of_day
-      @events_today = Event.order(:start_at).select {|x| x.end_at > d && x.end_at <= d.at_end_of_day }
+      @events_today = Event.where(end_at: d..d.at_end_of_day).order(:start_at)
       @left_name = "Tomorrow"
 
       if not @user.blank?
-        @events_today.select! {|e| groups.include? e.group.id }
+        @events_today = @events_today.where(group_id: groups)
       end
 
+      @events_today = @events_today.group(:title)
     end
-
-    #@events_today = @events_today.paginate(page: params[:page])
     
-    
-    @events_upcoming = Event.order(:start_at).select {|x| x.end_at > d.at_end_of_day }
-    # @events = Event.paginate(page: params[:page])
+    @events_upcoming = Event.where('end_at > :now', {now: d.at_end_of_day}).order(:start_at)
 
     if not @user.blank?
-      @events_upcoming.select! {|e| groups.include? e.group.id }
+      @events_upcoming = @events_upcoming.where(group_id: groups)
     end
 
+    @events_upcoming = @events_upcoming.group(:title)
+
     @events_upcoming = @events_upcoming.paginate(page: params[:page], per_page: 20)
-    
   end
 
 
