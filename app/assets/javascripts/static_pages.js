b0ba98bb7ecec8ea5b8e2caf13c469e1a89d5d4a
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-07 18:12:51 -0800

    infinite scroll is a thing

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,3 +3,52 @@ $(document).ready(function() {
         $(this).find('.desc').toggleClass('ellipsis');
     });
 });
+
+function isBottomVisible(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return (elemBottom <= docViewBottom + 100);
+}
+
+
+// from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+    pl     = /\+/g,  // Regex for replacing addition symbol with a space
+    search = /([^&=]+)=?([^&]*)/g,
+    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+    query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+var page = null;
+
+$(window).scroll(function() {
+    if(page === null) {
+        page = urlParams.page == undefined ? '1' : urlParams.page;
+        page = parseInt(page);
+    }
+
+    if(isBottomVisible($('#upcomingList'))) {
+        page += 1;
+        $('#loadingDiv').load('/?page=' + page + ' #upcomingList');
+        $('#upcomingList').append($('#loadingDiv').find('#upcomingList').html());
+
+        setTimeout(function()  {
+            $('.bigEvent').unbind('click');
+            $('.bigEvent').click(function() {
+                $(this).find('.desc').toggleClass('ellipsis');
+            });
+        }, 500);
+    }
+
+})
