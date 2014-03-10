bf169adb3a1a8412c93a96a289f4e7b7fb6c4f40
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-10 07:55:45 -0700

    add H@B events to events.csv

--- scripts/group_colors.py
+++ scripts/group_colors.py
@@ -3,17 +3,22 @@ import random
 import colorsys
 from data import *
 
+def generate_color():
+    h = random.uniform(0, 1)
+    s = random.uniform(0.6, 0.7)
+    v = random.uniform(0.45, 0.57)
+
+    rgb = colorsys.hsv_to_rgb(h, s, v)
+    rgb = tuple(map(lambda x: round(x*255), rgb))
+    col = '#%02X%02X%02X' % rgb
+
+    return col
+    
 def generate_group_colors():
     groups = map(lambda x: x[2], cal_ids)
     out = []
     for g in groups:
-        h = random.uniform(0, 1)
-        s = random.uniform(0.6, 0.7)
-        v = random.uniform(0.45, 0.57)
-
-        rgb = colorsys.hsv_to_rgb(h, s, v)
-        rgb = tuple(map(lambda x: round(x*255), rgb))
-        col = '#%02X%02X%02X' % rgb
+        col = generate_color()
 
         print((g,col))
         out.append( (g, col) )
@@ -24,3 +29,4 @@ def generate_group_colors():
     
 if __name__ == '__main__':
     generate_group_colors()
+    
