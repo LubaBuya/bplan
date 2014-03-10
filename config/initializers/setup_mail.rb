8953fd0149b5229beb70828d70b9bf48a97a6890
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-10 04:22:55 -0700

    better mail?

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "ucbplan.com",
+  :domain               => "ucbplan.herokuapp.com",
   :user_name            => ENV['EMAIL_USER'],
   :password             => ENV['EMAIL_PASSWORD'],
   :authentication       => "plain",
