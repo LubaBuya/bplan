d3cfef7d81815fc0cee2e28dae62d1ad5e5aac89
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-10 05:34:42 -0700

    try something...

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,8 @@
 Bplan::Application.config.action_mailer.delivery_method = :smtp
 
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
