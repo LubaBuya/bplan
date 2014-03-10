bb3cb25a867faa76fb025e85af45369a82cdd959
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-10 04:58:19 -0700

    test email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "ucbplan.herokuapp.com",
+  :domain               => "gmail.com",
   :user_name            => ENV['EMAIL_USER'],
   :password             => ENV['EMAIL_PASSWORD'],
   :authentication       => "plain",
