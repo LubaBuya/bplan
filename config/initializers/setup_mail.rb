31a066e57d0a7dd985807f230c308c257139ba3f
Pierre Karashchuk <krchtchk@gmail.com>
2014-03-10 05:26:32 -0700

    eplicit delivery method

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,5 @@
-config.action_mailer.delivery_method = :smtp
+Bplan::Application.config.action_mailer.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
